e5862fad7517ddeabcea3200d1c038b5115f124a
Patrick Koehn <53200243+mixolydianpink@users.noreply.github.com>
2025-09-14 09:37:46 -0400

    Organization.

--- pin1yin1-lib/pin1yin1/in.rkt
+++ pin1yin1-lib/pin1yin1/in.rkt
@@ -1,12 +1,29 @@
 #lang racket/base
 
-(provide pin1yin1-string->pin1yin1/rest
+(provide make-pin1yin1/p
+         pin1yin1-string->pin1yin1/rest
          pin1yin1-string->pin1yin1)
 
-(require racket/match
+(require (only-in racket/function
+                  curry)
+         racket/match
 
+         pin1yin1/non-phonetic/parse
          pin1yin1/parse
+         pin1yin1/phonetic/parse
          pin1yin1/pin1yin1/parse)
+
+(define (make-pin1yin1/p #:interpret-v-as-u-umlaut? interpret-v-as-u-umlaut?
+                         #:interpret-e^-as-e-circumflex? interpret-e^-as-e-circumflex?
+                         #:implicit-neutral-tone? implicit-neutral-tone?)
+  (pin1yin1/p #:compound/p
+              (compound/p (polysyllable/p #:implicit-neutral-tone? implicit-neutral-tone?
+                                          (curry syllable/p
+                                                 #:interpret-v-as-u-umlaut?
+                                                 interpret-v-as-u-umlaut?
+                                                 #:interpret-e^-as-e-circumflex?
+                                                 interpret-e^-as-e-circumflex?)))
+              #:non-phonetic/p non-phonetic/p))
 
 (define (pin1yin1-string->pin1yin1/rest #:interpret-e^-as-e-circumflex? [interpret-e^-as-e-circumflex? #t]
                                         #:interpret-v-as-u-umlaut? [interpret-v-as-u-umlaut? #t]
--- pin1yin1-lib/pin1yin1/pin1yin1/parse.rkt
+++ pin1yin1-lib/pin1yin1/pin1yin1/parse.rkt
@@ -1,28 +1,15 @@
 #lang racket/base
 
-(provide pin1yin1/p
-         make-pin1yin1/p)
+(provide pin1yin1/p)
 
 (require (only-in racket/function
                   curry)
 
          pin1yin1/list
-         pin1yin1/non-phonetic/parse
-         pin1yin1/parse
-         pin1yin1/phonetic/parse)
+         pin1yin1/parse)
 
-(define (pin1yin1/p #:compound/p compound/p)
+(define (pin1yin1/p #:compound/p compound/p
+                    #:non-phonetic/p non-phonetic/p)
   (map/p flatten1
          (alternating-symmetric/p compound/p
                                   non-phonetic/p)))
-
-(define (make-pin1yin1/p #:interpret-v-as-u-umlaut? interpret-v-as-u-umlaut?
-                         #:interpret-e^-as-e-circumflex? interpret-e^-as-e-circumflex?
-                         #:implicit-neutral-tone? implicit-neutral-tone?)
-  (pin1yin1/p #:compound/p
-              (compound/p (polysyllable/p #:implicit-neutral-tone? implicit-neutral-tone?
-                                          (curry syllable/p
-                                                 #:interpret-v-as-u-umlaut?
-                                                 interpret-v-as-u-umlaut?
-                                                 #:interpret-e^-as-e-circumflex?
-                                                 interpret-e^-as-e-circumflex?)))))
