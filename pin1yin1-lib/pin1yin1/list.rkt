7da17c9109a016bee57854ba275f8e56dd7b5bb3
Patrick Koehn <53200243+mixolydianpink@users.noreply.github.com>
2025-09-17 11:30:51 -0400

    Refactor `flatten1`.

--- pin1yin1-lib/pin1yin1/list.rkt
+++ pin1yin1-lib/pin1yin1/list.rkt
@@ -5,9 +5,11 @@
 (require (only-in racket/list
                   append-map))
 
-(define (flatten1 lst)
-  (append-map (λ (any)
-                (if (list? any)
-                    any
-                    (list any)))
-              lst))
+(define (->list any)
+  (if (list? any)
+      any
+      (list any)))
+
+(define (flatten1 list)
+  (append-map ->list
+              list))
